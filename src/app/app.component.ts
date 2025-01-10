34f3ab39be9e749678cd1c8eea2a8e8ef9f9c099
Gigabyets <ddos.wdsn@gmail.com>
2025-01-10 17:15:41 +0700

    sec.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { FooterComponent } from './footer/footer.component'; 
+import { FeaturesComponent } from './features/features.component'; 
+import { HeroComponent } from './hero/hero.component'; 
+import { HeaderComponent } from './header/header.component'; 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FooterComponent,FeaturesComponent,HeroComponent,HeaderComponent],
+  
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'miner_fortune';
+  
 }
